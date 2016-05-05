2abe24b4fe05045a0b7892f09578044883eecd50
lk547256398 <lk547256398@gmail.com>
2016-05-05 02:08:00 -0700

    modify unit test

--- UtilTime/main.cpp
+++ UtilTime/main.cpp
@@ -1,4 +1,4 @@
-/*#include <iostream>
+#include <iostream>
 #include "timer.h"
 
 using namespace util;
@@ -41,49 +41,4 @@ int main()
 	TestDefault();
 	TestPause();
 	return 0;
-}*/
-/*
-#define BOOST_TEST_MAIN
-#define BOOST_TEST_MODULE "timer"
-#include <boost/test/unit_test.hpp>
-
-#include <vector>
-
-#include "timer.h"
-
-using namespace util;
-
-BOOST_AUTO_TEST_CASE(TestDefault) {
-  Timer timer;
-  BOOST_CHECK_EQUAL(timer.ElapsedMicroSeconds(), 0);
-  BOOST_CHECK_EQUAL(timer.ElapsedSeconds(), 0);
-  BOOST_CHECK_EQUAL(timer.ElapsedMinutes(), 0);
-  BOOST_CHECK_EQUAL(timer.ElapsedHours(), 0);
 }
-
-BOOST_AUTO_TEST_CASE(TestStart) {
-  Timer timer;
-  timer.Start();
-  BOOST_CHECK_GE(timer.ElapsedMicroSeconds(), 0);
-  BOOST_CHECK_GE(timer.ElapsedSeconds(), 0);
-  BOOST_CHECK_GE(timer.ElapsedMinutes(), 0);
-  BOOST_CHECK_GE(timer.ElapsedHours(), 0);
-}
-
-BOOST_AUTO_TEST_CASE(TestPause) {
-  Timer timer;
-  timer.Start();
-  timer.Pause();
-  double prev_time = timer.ElapsedMicroSeconds();
-  for (size_t i = 0; i < 1000; ++i) {
-    BOOST_CHECK_EQUAL(timer.ElapsedMicroSeconds(), prev_time);
-    prev_time = timer.ElapsedMicroSeconds();
-  }
-  timer.Resume();
-  for (size_t i = 0; i < 1000; ++i) {
-    BOOST_CHECK_GE(timer.ElapsedMicroSeconds(), prev_time);
-  }
-  timer.Reset();
-  BOOST_CHECK_EQUAL(timer.ElapsedMicroSeconds(), 0);
-}
-*/
