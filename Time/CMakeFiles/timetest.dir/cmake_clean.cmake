4df0a4cd0a5ebde094ed3cd1629c499f5777cae3
lk547256398 <lk547256398@gmail.com>
2016-05-05 18:01:50 -0700

    modify time name

---
+++ Time/CMakeFiles/timetest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/timetest.dir/main.cpp.o"
+  "CMakeFiles/timetest.dir/timer.cc.o"
+  "timetest.pdb"
+  "timetest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/timetest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
