529dc4e812ad06339f61c6f0c3804256d6506049
lk547256398 <lk547256398@gmail.com>
2016-05-09 01:36:11 -0700

    add random and thread file

---
+++ Thread/CMakeFiles/TreadTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/TreadTest.dir/main.cc.o"
+  "CMakeFiles/TreadTest.dir/threading.cc.o"
+  "TreadTest.pdb"
+  "TreadTest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/TreadTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
